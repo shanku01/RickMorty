24de33879b228dab3ba61fa4727865065891858d
Shashank Pradhan <shashankpradhan911@gmail.com>
2021-02-25 03:08:41 +0530

    infinite scroll added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import {useState,useEffect,useCallback} from "react";
+import {useState,useEffect,useCallback,useRef} from "react";
 import debounce from 'lodash.debounce';
 import axios from 'axios';
+import InfiniteScroll from "react-infinite-scroller";
 
 
 function App() {
 const [contacts,setContacts] =useState([])
 const [data,setData] =useState([])
-const [name,setName] =useState("rick")
 const [info,setInfo] =useState({})
-const [hasMore,setHasMore] =useState(true)
-const [url,setUrl] = useState("https://rickandmortyapi.com/api/character/?name="+name+"&page=1")
-
-const debounceOnChange = useCallback(debounce(onChange,1000),[]);
-const debounceOnFetchMore = useCallback(debounce(OnFetchMore,1000),[]);
-
+const [name,setName] =useState("rick")
+const [page,setPage] =useState(1)
+const [myhasmore,setHasMore] =useState(true)
+const [url,setUrl] = useState("https://rickandmortyapi.com/api/character/?name="+name+"&page="+page)
 
 
-//Inializing contacts
 useEffect(()=>{
+  setPage(1)
+  setUrl("https://rickandmortyapi.com/api/character/?name="+name+"&page="+page)
   dataFetch(setContacts)
 },[name]);
 
 useEffect(()=>{
   dataFetch(setData)
-  setContacts([...contacts,...data])
+  if (JSON.stringify(contacts)!=JSON.stringify(data)) setContacts([...contacts,...data]);
 },[url]);
 
 //Changing fetched data on search
 function onChange(value){
   setName(value)
+  setHasMore(true)
 }
 
 //using axios to fetch data
@@ -36,7 +36,6 @@ function dataFetch(set){
   axios.get(url)
   .then((res)=>
   {
-    console.log(res.data.results)
     set(res.data.results);
     setInfo(res.data.info);
   })
@@ -45,11 +44,10 @@ function dataFetch(set){
 
 //fetching more data on scroll
 function OnFetchMore(){
-  console.log(info)
-  if (info.next){ 
     setUrl(info.next);
+  if(contacts.length==info.count){
+   setHasMore(false) 
   }
-  else setHasMore(false);
 }
 
   return (
@@ -59,16 +57,27 @@ function OnFetchMore(){
         placeholder="Rick, Morty,Jesus.."
         onChange={e => {
           e.preventDefault();
-          debounceOnChange(e.target.value)}}
+          onChange(e.target.value)}}
       />
-      <button onClick={OnFetchMore}>click me</button>
-      {info.next}
-      {/*Priting object*/}
+    
+    <div>
+        <InfiniteScroll
+            pageStart={0}
+            loadMore={OnFetchMore}
+            hasMore={myhasmore}
+            loader={<div className="loader" key={0}>Loading ...</div>}
+            useWindow={false}
+        >
+                      {/*Priting object*/}
       {contacts.map((item,key)=>{
         return <div key ={key}>
         <p>{item.name}</p>
         </div>
       })}
+        </InfiniteScroll>
+
+</div>
+      
     </div>
   );
 }
